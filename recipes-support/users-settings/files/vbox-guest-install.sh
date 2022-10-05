684e1a9dc87ec10b06e9e99db372849fd22c0cb0
Roberto A. Foglietta <roberto.foglietta@gmail.com>
2022-10-05 18:34:00 +0200

    vbox-guest-install.sh rewritten and tested
    Signed-off-by: Roberto A. Foglietta <roberto.foglietta@gmail.com>

--- recipes-support/users-settings/files/vbox-guest-install.sh
+++ recipes-support/users-settings/files/vbox-guest-install.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
+#
+# Copyright (c) Roberto A. Foglietta, 2022
+#
+# Authors:
+#  Roberto A. Foglietta <roberto.foglietta@gmail.com>
+#
+# SPDX-License-Identifier: MIT
+#
 
 test "$1" != "root" && sudo $0 root || exit 0
 
-apt update
-apt install dkms linux-headers-$(uname -r) build-essential virtualbox-guest-additions-iso
-mkdir -p /mnt/iso
-mount -o loop /usr/share/virtualbox/VBoxGuestAdditions.iso /mnt/iso
-bash /mnt/iso/VboxLinuxAdditions.run
-umount /mnt/iso
+apt update -f
+mkdir -p /mnt/cdrom
+mount /dev/cdrom /mnt/cdrom
+if ! ls -1 /mnt/cdrom/*.run >/dev/null 2>&1; then
+	apt install -y virtualbox-guest-additions-iso
+	mount -o loop /usr/share/virtualbox/VBoxGuestAdditions.iso /mnt/cdrom
+fi
+apt install -y dkms linux-headers-$(uname -r) build-essential virtualbox-guest-additions-iso bzip2
+bash $(ls -1 /mnt/cdrom/*.run || echo exit)
 usermod -aG vboxsf debraf
-echo "reboot, please"
