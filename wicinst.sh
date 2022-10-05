e19c0d4a20a29210ee7d0b52e7fffd30a3f87df0
Roberto A. Foglietta <roberto.foglietta@gmail.com>
2022-10-05 20:20:36 +0200

    wicinst.sh spaces in file names supported
    Signed-off-by: Roberto A. Foglietta <roberto.foglietta@gmail.com>

--- wicinst.sh
+++ wicinst.sh
@@ -12,15 +12,15 @@ cd $(dirname $0)
 
 for i in 1 2; do
 	if [ -b "$1" ]; then
-		bdev=$1
+		bdev="$1"
 	elif echo "$1" | grep -qe "^file:"; then
-		file=${1/file:/}
+		file="${1/file:/}"
 	elif echo "$1" | grep -qe "^pigz:"; then
-		pigz=${1/pigz:/}
+		pigz="${1/pigz:/}"
 	elif echo "$1" | grep -qe "^vmdk:"; then
-		vmdk=${1/vmdk:/}
+		vmdk="${1/vmdk:/}"
 	elif [ -e "$1" ]; then
-		fimg=$1
+		fimg="$1"
 	fi
 	shift
 done
@@ -45,17 +45,17 @@ if [ ! -e "$fimg" ]; then
 	exit 1
 fi
 
-size=$(du -b $fimg | cut -f1)
-szmb=$(du -m $fimg | cut -f1)
+size=$(du -b "$fimg" | cut -f1)
+szmb=$(du -m "$fimg" | cut -f1)
 echo
 echo "Transfering ${szmb}Mb: $fimg => ${bdev}${file}${pigz}${vmdk} ..."
 
 if [ -n "$bdev" ]; then
-	time (sudo dd if=$fimg bs=1M of=$bdev status=progress; sync)
+	time (sudo dd if="$fimg" bs=1M of=$bdev status=progress; sync)
 elif [ -n "$file" ]; then 
-	time (cp -paf $fimg $file; sync $file)
+	time (cp -paf "$fimg" "$file"; sync "$file")
 elif [ -n "$pigz" ]; then
-	time (pigz -c $fimg | dd bs=1M status=progress >$pigz; sync $pigz)
+	time (pigz -c "$fimg" | dd bs=1M status=progress >"$pigz"; sync "$pigz")
 elif [ -n "$vmdk" ]; then
 	if ! which qemu-img >/dev/null; then
 		echo
@@ -64,6 +64,6 @@ elif [ -n "$vmdk" ]; then
 		read key
 		sudo apt install -f qemu-utils
 	fi
-	time (qemu-img convert -pO vmdk $fimg $vmdk; sync $vmdk)
+	time (qemu-img convert -pO vmdk "$fimg" "$vmdk"; sync "$vmdk")
 fi
 echo
