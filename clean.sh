262b0db7d3752225ba43782a83f17f763be4954a
Roberto A. Foglietta <roberto.foglietta@gmail.com>
2023-01-17 10:52:18 +0100

    clean.sh notice about rootfs cache and support colors.shell
    Signed-off-by: Roberto A. Foglietta <roberto.foglietta@gmail.com>

--- clean.sh
+++ clean.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (c) Roberto A. Foglietta, 2022
+# Copyright (c) Roberto A. Foglietta, 2022-2023
 #
 # Authors:
 #  Roberto A. Foglietta <roberto.foglietta@gmail.com>
@@ -8,15 +8,41 @@
 # SPDX-License-Identifier: MIT
 #
 
+# using colors.shell or not
+NOTICE=${NOTICE:-NOTICE}
+USAGE=${USAGE:-USAGE}
+
 function print_help() {
+    local isar="${BLWHT:-}isar${CRST:-}" all="all"
+    test "$image_cache" != "active" && isar="isar" all="${BLWHT:-}all${CRST:-}"
+	echo "${USAGE}: $(basename $0) [ $all | $isar | kas | tmp ]"
 	echo
-	echo "USAGE: $(basename $0) [ all | isar | kas | tmp ]"
-	echo
-	echo "       usully the 'isar' is the target to clean"
+    test "$image_cache" != "active" && return
+	echo "       usually the '${isar}' is the target to clean"
 	echo
 }
 
-cd $(dirname $0)
+function getcache() {
+    local ovrloadfunc image_curnt image_templ image_cache
+    image_curnt="recipes-core/images/eval-image.bb"
+    image_templ="recipes-core/images/eval-image-template.inc"
+    if [ ! -r "$image_curnt" -a ! -r "$image_templ" ]; then
+       return 1
+    fi
+    ovrloadfunc="rootfs_install_sstate_prepare"
+    image_cache=$(grep -nw "$ovrloadfunc" "$image_templ" "$image_curnt" 2>/dev/null)
+    image_cache=$(echo "$image_cache" | cut -d: -f3- | cut -d '#' -f1)
+    image_cache=${image_cache:+disabled}
+    image_cache=${image_cache:-active}
+    echo $image_cache
+}
+
+cd "$(dirname $0)"
+
+image_cache=$(getcache)
+image_cache_msg=${image_cache/disabled/${DWHT:-}disabled${CRST}, use \'${BLWHT:-}clean all${CRST:-}\' only}
+image_cache_msg=${image_cache_msg/active/${BLGRN:-}active${CRST:-}}
+echo -e "\n${NOTICE}: the image rootfs is ${image_cache_msg}\n"
 
 case $1 in
 	all) sudo rm -rf $(ls -1d build/* 2>/dev/null | grep -ve "^build/downloads")
