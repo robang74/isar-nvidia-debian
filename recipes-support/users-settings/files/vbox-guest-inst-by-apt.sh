525919cfa05f948fc37783d557bf5f65836b387d
Roberto A. Foglietta <roberto.foglietta@gmail.com>
2023-01-29 09:01:07 +0100

    users settings: vbox-guest-inst-by-apt.sh improved and bugfixes
    Signed-off-by: Roberto A. Foglietta <roberto.foglietta@gmail.com>

--- recipes-support/users-settings/files/vbox-guest-inst-by-apt.sh
+++ recipes-support/users-settings/files/vbox-guest-inst-by-apt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (c) Roberto A. Foglietta, 2022
+# Copyright (c) Roberto A. Foglietta, 2022-2023
 #
 # Authors:
 #  Roberto A. Foglietta <roberto.foglietta@gmail.com>
@@ -8,17 +8,40 @@
 # SPDX-License-Identifier: MIT
 #
 
-test "$1" != "root" && sudo $0 root
-test "$1" != "root" && exit 0
+if [ "$1" != "root" ]; then
+        sudo $0 root "$@"
+        exit $?
+else
+        shift
+fi
+
+set -e
+trap 'echo "ERROR: in '$0' at line $LINENO, try to run with set -x"' ERR
+
 echo "Running $0 as $(whoami)"
 
-apt update
+mnt=$(mount | sed -ne "s,^/dev/sr0 on \([^ ]*\) .*,\\1,p")
+mnt=${mnt:-/mnt/cdrom}
+if [ "$mnt/VBoxLinuxAdditions.run" ]; then
+	echo
+	echo "WARNING: a VirtualBox guest install script is available at"
+	echo
+	echo "         $mnt/VBoxLinuxAdditions.run"
+	echo
+	echo -n "         are you sure do you want install by network (y/N) "
+	read ans
+	echo
+	if [ "$ans" != "y" -a "$ans" != "Y" ]; then
+		exit 1
+	fi
+fi
 mkdir -p /mnt/cdrom
-#mount /dev/cdrom /mnt/cdrom
-#if ! ls -1 /mnt/cdrom/*.run >/dev/null 2>&1; then
-	apt install -y virtualbox-guest-additions-iso
-	mount -o loop /usr/share/virtualbox/VBoxGuestAdditions.iso /mnt/cdrom
-#fi
-#apt install -y dkms linux-headers-$(uname -r) build-essential virtualbox-guest-additions-iso bzip2
-bash $(ls -1 /mnt/cdrom/*.run || echo exit)
-usermod -aG vboxsf debraf
+if mountpoint -q /mnt/cdrom; then
+	umount /mnt/cdrom
+fi
+
+apt update
+apt install -y virtualbox-guest-additions-iso dkms linux-headers-$(uname -r) build-essential bzip2
+mount -o loop -r /usr/share/virtualbox/VBoxGuestAdditions.iso /mnt/cdrom
+echo
+${0%/*}/vbox-guest-inst-by-sr0.sh
