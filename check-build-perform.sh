35e0ab915c48d198981161c582407c7c9c5946d3
Roberto A. Foglietta <roberto.foglietta@gmail.com>
2023-02-07 13:54:34 +0100

    check-build-perform.sh updated and aligned with devel3
    Signed-off-by: Roberto A. Foglietta <roberto.foglietta@gmail.com>

--- check-build-perform.sh
+++ check-build-perform.sh
@@ -1,43 +1,93 @@
 #!/bin/bash -m
 
 function fds() {
-  df -m / | grep /dev | tr -s ' ' | cut -d' ' -f4;
+    df -m / | grep /dev | tr -s ' ' | cut -d' ' -f4;
 }
 
 function ms() {
-  du -ms "$@" 2>/dev/null | cut -f1
+    du -ms "$@" 2>/dev/null | cut -f1
 }
 
 function chkfds() {
-    st=$(fds); mx=0;
-    while sleep 1; do ct=$[st-$(fds)];
-    if [ $ct -gt $mx ]; then
-        printf "%5s Mb\n" $ct
-        mx=$ct
-    fi >/dev/null
-    done; ct=$[st-$(fds)];
+    local st=$(fds) mx=0 ret=0
+    while [ $ret -eq 0 ]; do
+        ct=$[st-$(fds)];
+        if [ $ct -gt $mx ]; then
+            printf "%5s Mb\n" $ct
+            mx=$ct
+        fi >/dev/null
+        ret=$( (exec -ca "chkbldsleep" sleep 1); echo $? )
+    done
+    ct=$[st-$(fds)];
     echo
     printf "%5s Mb (max)\n" $mx;
     printf "%5s Mb (rest)\n" $ct;
+    printf "%5s Mb (tmp)\n" $(ms build/tmp);
     printf "%5s Mb (deb)\n" $(ms build/downloads/deb);
     printf "%5s Mb (wic)\n" $(ms build/tmp/deploy/images/*/*.wic);
     printf "%5s Mb (cache)\n" $(ms build/sstate-cache);
 }
 
 function killsleep() {
-    for i in $(seq 1 10); do
+    local p i
+    for i in $(seq 1 ${1:-10}); do
+        p=$(grep -nw "chkbldsleep" /proc/[1-9]*/cmdline 2>&1)
+        p=$(echo "$p" | cut -d/ -f3)
+        if [ -n "$p" ]; then
+            kill $p && kill $p && break
+        else
+            break
+        fi
         sleep 0.15
-        killall sleep && break
     done 2>/dev/null
+    fg 2>/dev/null
+    return 0
 }
 export -f killsleep
 
+function do_build_after_clean() {
+    local clean=$1
+    shift
+    ./clean.sh $clean
+    chkfds & ./build.sh "$@"
+    killsleep
+}
+export -f do_build_after_clean
+
+log="build.log"
+
 test -n "$1" && rm -f recipes-core/images/eval-image.bb
 trap "killsleep" EXIT
+killsleep 1
 
-echo "Cleaning all ..."
-./clean.sh all
-chkfds & ./build.sh "$@"
-killsleep
+test -e $log && mv -f $log $log.bak
+
+if [ ${CACHEONLY:-0} -ne 1 ]; then
+    echo
+    echo "Cleaning all for a fresh build with downloads untouched..."
+    if [ ${LOGBUILD:-0} -ne 0 ]; then
+        do_build_after_clean all "$@" || exit $?
+    fi 2>&1 | tee $log
+    if [ ${LOGBUILD:-0} -eq 0 ]; then
+        do_build_after_clean all "$@" || exit $?
+    fi
+fi
+
+if [ ${LOGBUILD:-0} -eq 0 ]; then
+    if [ ${CACHEONLY:-0} -ne 1 -a ${NOSTOP:-0} -ne 1 ]; then
+        echo
+        echo "Press ENTER to start the isar clean cached rebuild"
+        read
+    else
+        echo
+    fi
+    echo "Cleaning isar for rebuild supported by the cache..."
+    do_build_after_clean isar "$@" || exit $?
+fi
+if [ ${LOGBUILD:-0} -ne 0 ]; then
+    echo "Cleaning isar for rebuild supported by the cache..."
+    do_build_after_clean isar "$@" || exit $?
+fi 2>&1 | tee -a $log
+
 trap - EXIT
 echo
